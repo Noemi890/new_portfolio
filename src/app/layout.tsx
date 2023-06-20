3c18d995c546b5c6df7839f5c2ff0e1b9aeb55eb
Noemi Caggiano <noemi.caggiano@gmail.com>
2023-06-20 12:28:38 +0100

    feat: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "./ui/Header/Header";
-import Footer from "./ui/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
+      <body className={`${inter.className} font-sans antialiased text-gray-100 leading-normal tracking-wider bg-cover bg-[url('https://source.unsplash.com/1L71sPT5XKc')]`} >
         <div>{children}</div>
-        <Footer />
       </body>
     </html>
   );
